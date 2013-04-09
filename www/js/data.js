5222aec55c307c2d4d58e886d918ddea28dfeb07
Andre Fourie <andre@nirph.com>
2013-04-09 20:10:58 +0200

    Debugging

--- www/js/data.js
+++ www/js/data.js
@@ -563,9 +563,8 @@ var Data = {
           filter[item.filter] = Config.data[item.filter] ? Config.data[item.filter] : null;
         }
         Data.listSynchData(Table[objName], filter, item.server_time, item.mode, function(table, synchData) {
-          alert(listSynchData + ': ' + table + ' >> ' + JSON.stringify(synchData));
           Server.post('data/synch', synchData, function(jsonResult) {
-            alert('Server result: ' + JSON.stringify(jsonResult));
+            alert(JSON.stringify(jsonResult));
             // Update local entries with relevant server id's
             for (var retObjName in jsonResult.Data) {
               var table = Table[retObjName];
